31bec646bca29993e8166889bd240d33e92eaa39
AC-Galen <lottaie7359882@gmail.com>
2022-05-17 15:48:34 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exhbs = require('express-handlebars')
 
 const app = express()
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
+
 const db = mongoose.connection
-// 連線異常
+
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.render('index')
 })
+
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
