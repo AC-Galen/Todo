7f87a1c7871897312f0d92415a0ed75290878d11
AC-Galen <lottaie7359882@gmail.com>
2022-06-05 16:18:27 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const todos = require('./modules/todos')
 const home = require('./modules/home')
+const users = require('./modules/users')
 
 //將網址結構符合的字串 導向模組
 router.use('/', home)
 router.use('/todos', todos)
+router.use('/users', users)
 
 //匯出路由器
 module.exports = router
