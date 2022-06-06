5cbbb3c2437a3da66745c5f3bd542f490f07f24b
AC-Galen <lottaie7359882@gmail.com>
2022-06-06 19:35:30 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const session = require('express-session')
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-//若在 Heroku 環境中使用process.nev.PORT，否則在本地環境(3000)
-const PORT = process.env.PORT || 3000
 
 const routes = require('./routes')
 require('./config/mongoose')
 
-
 const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(session ({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
