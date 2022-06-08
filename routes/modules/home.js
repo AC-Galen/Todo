c0855ae280db737c63b28abf2f3724150fcbe7c6
AC-Galen <lottaie7359882@gmail.com>
2022-06-08 15:04:17 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-//引用Todo model 內部資料
 const Todo = require('../../models/todo')
 
-//定義首頁路由
+
 router.get('/', (req, res) => {
-  Todo.find()
+  const userId = req.user._id
+  Todo.find({ userId })
     .lean()
     .sort({ _id: 'asc' })
     .then(todos => res.render('index', { todos }))
     .catch(error => console.log(error))
 })
 
-//匯出路由模組
 module.exports = router
